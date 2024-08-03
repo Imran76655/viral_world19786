032966ab8df1187731ad5203b9e43b0eb4760a88
Imran76655 <shamimkhatri870@gmail.com>
2024-08-03 23:12:48 +0530

    Update 2477-0c97286f9ce7ea50.js

--- 2477-0c97286f9ce7ea50.js
+++ 2477-0c97286f9ce7ea50.js
@@ -4320,7 +4320,7 @@
                         try {
                             (JSON.parse(t).quota_limited || []).forEach(function(e) {
                                 G.info("[RateLimiter] ".concat(e || "events", " is quota limited.")),
-                                r.serverLimits[e] = (new Date).getTime() - 6e4
+                                //r.serverLimits[e] = (new Date).getTime() - 6e4
                             })
                         } catch (e) {
                             return void G.warn('[RateLimiter] could not rate limit - continuing. Error: "'.concat(null == e ? void 0 : e.message, '"'), {
